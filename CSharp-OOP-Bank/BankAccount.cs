2e986c32c2cb834711dd2b35148997fba47ae6cb
Cal-Xavi <callummander@gmail.com>
2022-10-28 13:52:35 +0100

    initial commit

---
+++ CSharp-OOP-Bank/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP_Bank
+{
+    internal class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeeded = 1234567890;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Deposit");
+
+            this.Number = accountNumberSeeded.ToString();
+            accountNumberSeeded++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            
+          if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit should be positive. Program will now exit.");
+            }
+
+          var deposit = new Transaction(amount, date, note);
+          allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal should be positive. Program will now exit.");
+            }
+
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient funds for this withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+}
---
+++ CSharp-OOP-Bank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp_OOP_Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
